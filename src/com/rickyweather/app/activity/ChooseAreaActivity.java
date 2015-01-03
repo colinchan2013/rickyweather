697982ef4d3cadeadcf8e6cf8363e81399eac4b7
Rricky <studycolin@gmail.com>
2015-01-03 23:58:29 +0800

    增加读取天气代码

---
+++ src/com/rickyweather/app/activity/ChooseAreaActivity.java
@@ -0,0 +1,244 @@
+package com.rickyweather.app.activity;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.view.Window;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.rickyweather.app.R;
+import com.rickyweather.app.model.City;
+import com.rickyweather.app.model.County;
+import com.rickyweather.app.model.Province;
+import com.rickyweather.app.model.RickyWeatherDB;
+import com.rickyweather.app.util.HttpCallbackListener;
+import com.rickyweather.app.util.HttpUtil;
+import com.rickyweather.app.util.Utility;
+
+public class ChooseAreaActivity extends Activity {
+	public static final int LEVEL_PROVINCE = 0;
+	public static final int LEVEL_CITY = 1;
+	public static final int LEVEL_COUNTY = 2;
+
+	private ProgressDialog progressDialog;
+	private TextView titleText;
+	private ListView listView;
+	private ArrayAdapter<String> adapter;
+	private RickyWeatherDB rickyWeatherDB;
+	private List<String> dataList = new ArrayList<>();
+
+	/**
+	 * 省列表
+	 */
+	private List<Province> provinceList;
+	/**
+	 * 市列表
+	 */
+	private List<City> cityList;
+	/**
+	 * 县列表
+	 */
+	private List<County> countyList;
+	/**
+	 * 选中的省份
+	 */
+	private Province selectedProvince;
+	/**
+	 * 选中的城市
+	 */
+	private City selectedCity;
+	/**
+	 * 选中的级别
+	 */
+	private int currentLevel;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+		setContentView(R.layout.choose_area);
+		listView = (ListView) findViewById(R.id.list_view);
+		titleText = (TextView) findViewById(R.id.title_text);
+		adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1,
+				dataList);
+		listView.setAdapter(adapter);
+		rickyWeatherDB = rickyWeatherDB.getInstance(this);
+		listView.setOnItemClickListener(new OnItemClickListener() {
+
+			@Override
+			public void onItemClick(AdapterView<?> arg0, View arg1, int arg2,
+					long arg3) {
+				// TODO Auto-generated method stub
+				if (currentLevel == LEVEL_PROVINCE) {
+					selectedProvince = provinceList.get(arg2);
+				} else if (currentLevel == LEVEL_CITY) {
+					selectedCity = cityList.get(arg2);
+					queryCounties();
+				}
+
+			}
+		});
+		queryProvinces();// 加载省级数据
+	}
+
+	/**
+	 * 查询全国所有省，优先从数据库查询，如果没有查询到再去服务器上查询
+	 */
+	private void queryProvinces() {
+		// TODO Auto-generated method stub
+		provinceList = rickyWeatherDB.loadProvinces();
+		if (provinceList.size() > 0) {
+			dataList.clear();
+			for (Province province : provinceList) {
+				dataList.add(province.getProvinceName());
+			}
+			adapter.notifyDataSetChanged();
+			listView.setSelection(0);
+			titleText.setText("中国");
+			currentLevel = LEVEL_PROVINCE;
+		} else {
+			queryFromServer(null, "province");
+		}
+
+	}
+
+	/**
+	 * 查询全国省内所有市，优先从数据库查询，如果没有查询到再去服务器上查询
+	 */
+	private void queryCities() {
+		// TODO Auto-generated method stub
+		cityList = rickyWeatherDB.loadCities(selectedProvince.getId());
+		if (cityList.size() > 0) {
+			dataList.clear();
+			for (City city : cityList) {
+				dataList.add(city.getCityName());
+			}
+			adapter.notifyDataSetChanged();
+			listView.setSelection(0);
+			titleText.setText(selectedProvince.getProvinceName());
+			currentLevel = LEVEL_CITY;
+		} else {
+			queryFromServer(selectedProvince.getProvinceCode(), "city");
+		}
+
+	}
+
+	/**
+	 * 查询全国市里面所有县，优先从数据库查询，如果没有查询到再去服务器上查询
+	 */
+	private void queryCounties() {
+		// TODO Auto-generated method stub
+		countyList = rickyWeatherDB.loadCounties(selectedCity.getId());
+		if (countyList.size() > 0) {
+			dataList.clear();
+			for (County county : countyList) {
+				dataList.add(county.getCountyName());
+			}
+			adapter.notifyDataSetChanged();
+			listView.setSelection(0);
+			titleText.setText(selectedCity.getCityName());
+			currentLevel = LEVEL_COUNTY;
+		} else {
+			queryFromServer(selectedCity.getCityCode(), "county");
+		}
+
+	}
+
+	/**
+	 * 根据传入的代号和类型从服务器上查询省市县数据。
+	 * 
+	 * @param Code
+	 * @param string
+	 */
+
+	private void queryFromServer(final String code, final String type) {
+		// TODO Auto-generated method stub
+		String address;
+		if (!TextUtils.isEmpty(code)) {
+			address = "http://www.weather.com.cn/data/list3/city" + code
+					+ ".xml";
+		} else {
+			address = "http://www.weather.com.cn/data/list3/city.xml";
+
+		}
+		showProgressDialog();
+		HttpUtil.sendHttpRequest(address, new HttpCallbackListener() {
+
+			@Override
+			public void onFinish(String response) {
+				// TODO Auto-generated method stub
+				boolean result = false;
+				if ("province".equals(type)) {
+					result = Utility.handleProvinceResponse(rickyWeatherDB,
+							response);
+
+				} else if ("city".equals(type)) {
+					result = Utility.handleCitiesResponse(rickyWeatherDB,
+							response, selectedProvince.getId());
+				} else if ("county".equals(type)) {
+					result = Utility.handleCountiesResponse(rickyWeatherDB,
+							response, selectedCity.getId());
+				}
+				if (result) {
+					runOnUiThread(new Runnable() {
+
+						@Override
+						public void run() {
+							closeProgressDialog();
+							if ("province".equals(type)) {
+								queryProvinces();
+							} else if ("city".equals(type)) {
+								queryCities();
+							} else if ("county".equals(type)) {
+								queryCounties();
+							}
+						}
+					});
+				}
+			}
+
+			@Override
+			public void onError(Exception e) {
+				// TODO Auto-generated method stub
+				runOnUiThread(new Runnable() {
+
+					@Override
+					public void run() {
+						closeProgressDialog();
+						Toast.makeText(ChooseAreaActivity.this, "加载失败",
+								Toast.LENGTH_LONG).show();
+					}
+
+				});
+			}
+		});
+	}
+
+	/**
+	 * 显示进度对话框
+	 */
+	private void showProgressDialog() {
+		// TODO Auto-generated method stub
+
+	}
+
+	/**
+	 * 关闭进度对话框
+	 */
+	private void closeProgressDialog() {
+		// TODO Auto-generated method stub
+
+	}
+}
